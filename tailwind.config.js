2b898c0fbd208092beaa2eaa796ada718354c2c6
Your Company Name <diptanshu.bhawsar@kimp.xyz>
2024-10-16 21:49:22 +0530

    fix preset issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require('nativewind/preset')],
   content: [
-    './app/**/*.{js,jsx,ts,tsx}',
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
-  // presets: [require('tailwindcss-react-native/preset')],
   theme: {
     extend: {},
   },
